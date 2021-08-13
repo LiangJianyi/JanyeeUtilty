e5b4194124a39eedc5385925b818c6563b715d3c
LiangJianyi <a.124p@live.com>
2021-08-13 08:04:47 +0800

    Changesets-120

--- Sources/Graph/Topological.swift
+++ Sources/Graph/Topological.swift
@@ -1,5 +1,18 @@
 import Foundation
+import Sequence
 
 public class Topological {
+    public private(set) var order: Stack<Int, LinkedList<Int>>?
     
+    public var isDAG: Bool {
+        return self.order != nil
+    }
+    
+    public init(digraph: DirectedGraph) {
+        let cycleFinder = DirectedCycle(graph: digraph)
+        if cycleFinder.hasCycle == false {
+            let dfo = DepthFirstOrder(graph: digraph)
+            self.order = dfo.reversePost
+        }
+    }
 }
