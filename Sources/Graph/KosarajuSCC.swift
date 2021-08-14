787dcabf0e23b2cc382f1237f20d664cb95138c7
LiangJianyi <a.124p@live.com>
2021-08-14 15:48:41 +0800

    Changesets-121

---
+++ Sources/Graph/KosarajuSCC.swift
@@ -0,0 +1,32 @@
+public class KosarajuSCC {
+    private var marked: [Bool]
+    
+    public private(set) var id: [Int]
+    public private(set) var count: Int = 0
+    
+    init(digraph: DirectedGraph) {
+        self.marked = [Bool](repeating: false, count: digraph.vertex)
+        self.id = [Int](repeating: 0, count: digraph.vertex)
+        let order = DepthFirstOrder(graph: digraph.reverse())
+        for s in order.reversePost {
+            if self.marked[s] == false {
+                self.dfs(digraph: digraph, v: s)
+                self.count += 1
+            }
+        }
+    }
+    
+    private func dfs(digraph: DirectedGraph, v: Int) {
+        self.marked[v] = true
+        self.id[v] = count
+        for w in digraph.adjust[v] {
+            if self.marked[w] == false {
+                dfs(digraph: digraph, v: w)
+            }
+        }
+    }
+    
+    public func stronglyConnected(v: Int, w: Int) -> Bool {
+        return self.id[v] == self.id[w]
+    }
+}
